31d18563495a737850e0f7e417ab1de14ae12e8e
Aresh Tajvar <tajvararesh@gmail.com>
2024-12-02 09:46:22 -0800

    Updated the following files:     data_exploration.py     requirements.txt     .gitignore

--- data_exploration.py
+++ data_exploration.py
@@ -1,10 +1,9 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from data_loader import load_data
 
-# Load original dataframe created in data_loader.py
-gtdb_df = load_data()
-
-""" Section: Clean Dataframe """
 def column_dropper(df, threshold):
     """
     Drops columns from a DataFrame that have more than the specified threshold of missing values.
@@ -19,33 +18,45 @@ def column_dropper(df, threshold):
 
     missing_values_percent = df.isnull().sum() * 100 / len(df)
     columns_to_drop = missing_values_percent[missing_values_percent > threshold].index
+
     print(f"Dropping columns with more than {threshold * 100}% missing values:")
     for col in columns_to_drop:
         print(f"- {col}")
+
     return df.drop(columns_to_drop, axis=1)
 
-df_cleaned = column_dropper(gtdb_df, 0.9)  # Drop columns with >90% missing values
+with open('output.txt', 'w') as f:
+    original_stdout = sys.stdout
+    sys.stdout = f
 
-# Print first five rows of df_cleaned
-print(df_cleaned.head(5))
+    # Load original dataframe created in data_loader.py
+    gtdb_df = load_data()
 
-# Print dataframe shape
-print(df_cleaned.shape)
+    df_cleaned = column_dropper(gtdb_df, 0.9)  # Drop columns with >90% missing values
 
-""" Section: Explore Data """
-import matplotlib.pyplot as plt
-import numpy as np
+    # Print first five rows of df_cleaned
+    print(df_cleaned.head(5), file=f)
 
-# Select numerical columns
-numerical_columns = df_cleaned.select_dtypes(include='number').columns
+    # Print dataframe shape
+    print(df_cleaned.shape, file=f)
 
-# Create individual box plots for each numerical column of data
-fig, axes = plt.subplots(nrows=int(np.ceil(len(numerical_columns) / 4)), ncols=4, figsize=(12, 8))
+    """ Section: Explore Data """
 
-for i, column in enumerate(numerical_columns): 
-    row, col = i // 4, i % 4
-    axes[row, col].boxplot(df_cleaned[column]) 
-    axes[row, col].set_title(column)
+    # Select numerical columns
+    numerical_columns = df_cleaned.select_dtypes(include='number').columns
 
-plt.tight_layout()
-plt.show()
+    # Create individual box plots for each numerical column of data
+    fig, axes = plt.subplots(nrows=int(np.ceil(len(numerical_columns) / 4)), ncols=4, figsize=(12, 8))
+
+    for i, column in enumerate(numerical_columns):
+        row, col = i // 4, i % 4
+        axes[row, col].boxplot(df_cleaned[column])
+        axes[row, col].set_title(column)
+
+    plt.tight_layout()
+
+    # Save the plot to a file (replace 'my_plot.png' with desired filename)
+    plt.savefig('my_plot.png')
+    print(f"Plot saved to: my_plot.png\n", file=f)
+
+sys.stdout = original_stdout
